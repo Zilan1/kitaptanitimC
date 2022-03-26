ad1e7973fe5b87a5b39e3f043eb5c70c0d139124
Zilan1 <79017444+Zilan1@users.noreply.github.com>
2022-03-26 16:02:40 +0300

    sum

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	char kitapad [20]= "Ucurtma Avcisi";
+	char yazar [15]="Halit Huseyni";
+	char turu [5]="Roman";
+    char sayfa[3]="375";
+	char basimyili[4]="2003";
+	
+	printf("*****Kitap Tanitim*****\n\n\n"); 
+	printf("Kitap adi: %s\n",kitapad);
+	printf("Kitap yazari: %s\n",yazar);
+	printf("Turu: %s\n",turu);
+	printf("Sayfa Sayisi: %s\n",sayfa);
+    printf("Basim Yili: %s\n",basimyili);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
